0bccc628f360f1f37f6f68a3f228920d19956564
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-09-23 23:02:12 +0900

    백준 반복문
    반복문 일부

---
+++ SolveBoj/boj_10950/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n, a, b, sum;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+
+    return 0;
+}
---
+++ SolveBoj/boj_15552/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, n;
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+
+
+    return 0;
+}
